699bdb072b2a4e51a8b76aef85b738249704a1d6
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-03-28 05:52:03 -0400

    devuelve la suma de 4 numeros de una cadena. Los numeros estan separados por una coma

--- src/calculadora.spec.js
+++ src/calculadora.spec.js
@@ -12,4 +12,8 @@ describe("Sumar", () => {
   it("deberia sumar dos numeros de una cadena separados por coma", () => {
     expect(calculadoraCadenas("1,3")).toEqual(4);
   });
+
+  it("deberia sumar 4 numeros de una cadena separados por coma", () => {
+    expect(calculadoraCadenas("1,3,4,2")).toEqual(10);
+  });
 });
